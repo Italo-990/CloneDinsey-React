9d4b1b40b116f2c3b0489e5892e58970c9866997
Italo-990 <italo.ribeiro2605@gmail.com>
2023-04-18 19:31:58 +0000

    navBarUpdate

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -2,23 +2,23 @@ import data from "./Data.json"
 import "./NavBar.css"
 function NavBar() {
   return (
-    <header className="header-page">
+    <header className="header-page flex-item">
       <img src={data.logoPage} className="logoPage"/>
       <nav className="nav-header">
-        <ul className="nav-header-ul">
+        <ul className="nav-header-ul flex-item flex-itens-center">
           {data.navData.map( ( { content, icon } )=>{
             return (
-              <li key={content} className="nav-li-header">
+              <li key={content} className="nav-li-header flex-item flex-itens-center">
                 <img className="nav-icon" src={icon}/>
-                <span>{content}</span>
+                <span className="span-nav">{content}</span>
               </li>
             )
           })}
         </ul>
       </nav>
-      <div className="header-right-content">
-        <span>{data.navRigth.name}</span>
-        <img src={data.navRigth.icon}></img>
+      <div className="header-right-content flex-item flex-item-center">
+        <span className="header-right-span">{data.navRigth.name}</span>
+        <img className="header-right-img" src={data.navRigth.icon}></img>
       </div>
     </header>
   )

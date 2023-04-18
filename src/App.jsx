2f2ee77e55f7494dd50b45d3a251fb67f379b396
Italo-990 <italo.ribeiro2605@gmail.com>
2023-04-18 13:32:04 +0000

    header-update

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import filmes from  "./ApiRes"
 import Banner from "./Components/Banner/Banner"
 import "./App.css"
 import RowOriginals from "./Components/RowOriginals/RowOriginals"
-
+import NavBar from "./Components/NavBar/NavBar"
 
 function App() {
   return (
       <>
+        <NavBar/>
         <Banner/>
         <RowOriginals/>
         {
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,6 +1,23 @@
+import data from "./Data.json"
+import "./NavBar.css"
 function NavBar() {
   return (
-    <div>NavBar</div>
+    <header className="header-page">
+      <img src={data.logoPage} className="logoPage"/>
+      <nav className="nav-header">
+        <ul className="nav-header-ul">
+          {data.navData.map( ( { content, icon } )=>{
+            return (
+              <li key={content} className="nav-li-header"><img className="nav-icon" src={icon}/>{content}</li>
+            )
+          })}
+        </ul>
+      </nav>
+      <div className="header-right-content">
+        <span>{data.navRigth.name}</span>
+        <img src={data.navRigth.icon}></img>
+      </div>
+    </header>
   )
 }
 
